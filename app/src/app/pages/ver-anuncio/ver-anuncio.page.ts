1af0f85266219246dc1f0aa6e70dd92a33a47cd3
Omarsd <omarsdsramo@gmail.com>
2020-05-23 17:39:51 +0200

    Añadido si no es tu anuncio no ves los botones de editar ni borrar

--- app/src/app/pages/ver-anuncio/ver-anuncio.page.ts
+++ app/src/app/pages/ver-anuncio/ver-anuncio.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Anuncio } from 'src/app/model/Anuncio';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FirebaseService } from 'src/app/services/anuncio.service';
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
 selector: 'app-ver-anuncio',
@@ -12,17 +13,40 @@ styleUrls: ['./ver-anuncio.page.scss'],
 export class VerAnuncioPage implements OnInit {
 
 	anuncio: Anuncio = {
-		id: '',
-		titulo: '',
-		descripcion: '',
-		createdAt: ''
-	};
+		id : '',
+		idMusico : '',
+    	titulo:  '',
+		descripcion:  '',
+		createdAt:  '',
+		fechaEvento:  null,
+		ubicacion:  '',
+		instrumento:  '',
+		tipoDemanda:  null,
+	}
+
+	owned: boolean;
+	uid: string;
 
 	constructor(private activatedRoute: ActivatedRoute,
 		private fbService: FirebaseService,
-		private router: Router) { }
+		private router: Router,
+		private AFauth: AngularFireAuth ) { }
 
 	ngOnInit() {
+		this.AFauth.auth.onAuthStateChanged(
+			user => {
+			  if (user) {
+				// User is signed in.
+				this.uid = user.uid
+				console.log(this.uid)
+			  }
+			  else {
+				// No user is signed in.
+				console.log("el usuario no ha iniciado sesion.");
+			  }
+			}
+		  );
+	  
 	}
 
 	ngAfterViewInit(): void {
@@ -31,6 +55,11 @@ export class VerAnuncioPage implements OnInit {
 			this.fbService.getAnuncio(id).subscribe(
 				data => {
 					this.anuncio = data;
+					if(data.idMusico == this.uid){
+						this.owned = true
+					} else{
+						this.owned = false
+					}
 				},
 				err => {
 					this.router.navigate(['/no-encontrado'])
