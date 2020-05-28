d8c3b6c3324937c0b58dd5d591ee962b26db31b7
DiazOscar <45843871+DiazOscar@users.noreply.github.com>
2020-05-28 12:02:25 +0200

    Menu lateral

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -10,12 +10,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  navigate : any;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.sideMenu();
     this.initializeApp();
   }
 
@@ -24,6 +26,24 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  sideMenu()
+  {
+    this.navigate =
+    [
+      {
+        title : "Home",
+        url   : "/home",
+        icon  : "home"
+      },
+      {
+        title : "About",
+        url   : "/about",
+        icon  : "chatboxes"
+      },
+      
+    ]
   }
 
 }
