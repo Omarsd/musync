e97c84ca93235a333a5910bcf2d845996b943029
DiazOscar <45843871+DiazOscar@users.noreply.github.com>
2020-06-06 17:19:05 +0200

    Tests unitarios modificados

--- app/src/app/pages/login/login.page.spec.ts
+++ app/src/app/pages/login/login.page.spec.ts
@@ -4,21 +4,5 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
-  let component: LoginPage;
-  let fixture: ComponentFixture<LoginPage>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(LoginPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
