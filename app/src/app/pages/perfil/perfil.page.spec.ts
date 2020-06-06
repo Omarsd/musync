e3d695939c0e7fb655c405e07f18807cd1585d46
RubenCastro <47066824+rubencastro24@users.noreply.github.com>
2020-06-06 22:00:35 +0200

    Test función esValido()

--- app/src/app/pages/perfil/perfil.page.spec.ts
+++ app/src/app/pages/perfil/perfil.page.spec.ts
@@ -3,6 +3,43 @@ import { IonicModule } from '@ionic/angular';
 
 import { PerfilPage } from './perfil.page';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { ModalController } from "@ionic/angular";
+import { AnunciosService } from 'src/app/services/anuncio.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { AuthService } from 'src/app/services/auth.service';
+
 describe('PerfilPage', () => {
-  
+	const AFauth = <AngularFireAuth> {}
+	const anuncioService = <AnunciosService> {}
+	const authServ = <AuthService> {}
+	const usuarioService = <UsuarioService> {}
+	const activatedRoute = <ActivatedRoute> {}
+	const router = <Router> {}
+	const modal = <ModalController> {}
+
+	let perfilPage: PerfilPage = new PerfilPage(AFauth, anuncioService, authServ, usuarioService, activatedRoute, router, modal)
+
+
+	describe('esValido(texto: String): Boolean', () => {
+		let texto : String;
+		it('texto = null => false', async(() => {
+			texto = null;
+			expect( perfilPage.esValido(texto) ).toBe(false);
+		}))
+		it('texto = undefined => false', async(() => {
+			texto = undefined;
+			expect( perfilPage.esValido(texto) ).toBe(false);
+		}))
+		it('texto = "" => false', async(() => {
+			texto = "";
+			expect( perfilPage.esValido(texto) ).toBe(false);
+		}))
+		it('texto = "Texto de prueba" => true', async(() => {
+			texto = "Texto de prueba";
+			expect( perfilPage.esValido(texto) ).toBe(true);
+		}))
+	});
+
 });
