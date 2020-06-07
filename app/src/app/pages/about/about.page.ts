d8154129dc15e995d76d1a34cd035e045f378078
oscarbr97 <62023821+oscarbr97@users.noreply.github.com>
2020-06-07 21:53:24 +0200

    Update about.page.ts

--- app/src/app/pages/about/about.page.ts
+++ app/src/app/pages/about/about.page.ts
@@ -38,7 +38,7 @@ export class AboutPage implements OnInit {
 			git: 'JuanmaKsp'
 		},
 		{
-			imagen: 'Oscarbr.png',
+			imagen: 'Oscar.png',
 			nombre: 'Óscar',
 			apellidos: 'Bracero Ramírez',
 			rol: 'Tester',
